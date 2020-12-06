baf11e20531ef5323e0ebcb18b6d3d732c693350
Celeter <48249130+Celeter@users.noreply.github.com>
2020-12-06 19:22:49 +0800

    更新

--- .github/scripts/API_lanzou.py
+++ .github/scripts/API_lanzou.py
@@ -1,14 +1,8 @@
 from lanzou.api import LanZouCloud
 import os
 
-# https://github.com/zaxtyson/LanZouCloud-API/blob/master/lanzou/api/core.py
-
 lzy = LanZouCloud()
 
-
-# print('正常:{}, URL错误:{}, 网络异常:{}, 文件已取消:{}, 缺少提取码:{}, 提取码错误:{}, 验证码错误:{}'.format(
-# 	LanZouCloud.SUCCESS, LanZouCloud.URL_INVALID, LanZouCloud.NETWORK_ERROR, LanZouCloud.FILE_CANCELLED, 
-# 	LanZouCloud.LACK_PASSWORD, LanZouCloud.PASSWORD_ERROR, LanZouCloud.CAPTCHA_ERROR))
 
 def get_result(code):
     if code == LanZouCloud.SUCCESS:
@@ -17,13 +11,10 @@ def get_result(code):
         return f'失败,errorCode:{code}'
 
 
-def login():
+def login(ylogin, phpdisk_info):
     print('开始登入')
-    ylogin = os.environ["LANZOU_ID"]
-    phpdisk_info = os.environ["LANZOU_PSD"]
     cookie = {'ylogin': ylogin, 'phpdisk_info': phpdisk_info}
     code = lzy.login_by_cookie(cookie)
-    #print('login:', code)
     print('登入结果:', get_result(code))
     return code
 
@@ -31,35 +22,33 @@ def login():
 def logout():
     print('开始登出')
     code = lzy.logout()
-    #print('logout:', code)
     print('登出结果:', get_result(code))
-    return lcode
+    return code
 
 
 def set_max_size(size=500):
     print('开始设置单文件大小上限')
     # 设置单文件大小限制
     code = lzy.set_max_size(size)
-    #print('set_max_size:', code)
     print('设置单文件大小上限结果:', get_result(code))
     return code
 
 
 def show_progress(file_name, total_size, now_size):
-    #print("进入进度回调函数")
+    # print("进入进度回调函数")
     percent = now_size / total_size
     bar_len = 50  # 进度条长总度
     bar_str = '>' * round(bar_len * percent) + '=' * round(bar_len * (1 - percent))
-    #print('\r{:.2f}%\t[{}] {:.1f}/{:.1f}MB | {} '.format(
+    # print('\r{:.2f}%\t[{}] {:.1f}/{:.1f}MB | {} '.format(
     #    percent * 100, bar_str, now_size / 1048576, total_size / 1048576, file_name), end='')
     print('\r{:.2f}%\t[{}] {:.1f}/{:.1f}MB '.format(
         percent * 100, bar_str, now_size / 1048576, total_size / 1048576), end='')
-    #if total_size == now_size:
+    # if total_size == now_size:
     #    print('任务完成')
 
 
 def handler(fid, is_file):
-    #print("进入上传回调函数")
+    # print("进入上传回调函数")
     if is_file:
         # 设置描述信息
         code = lzy.set_desc(fid, 'Legado Google Play Version', is_file=True)
@@ -71,8 +60,9 @@ def handler(fid, is_file):
 
 def upload(path, id):
     print('开始上传文件')
+    # 解除大小限制
+    lzy.ignore_limits()
     code = lzy.upload_file(path, id, callback=show_progress, uploaded_handler=handler)
-    #print('upload:', code)
     print('文件上传结果:', get_result(code))
     return code
 
@@ -83,10 +73,19 @@ def get_dir_list():
     print(dirs)
 
 
-if __name__ == "__main__":
-    if login() == LanZouCloud.SUCCESS:
-        # set_max_size()
+def get_apk_path(path):
+    if os.path.isfile(path):
+        return os.path.abspath(path)
+    else:
+        file_list = os.listdir(path)
+        for f in file_list:
+            if os.path.splitext(f)[-1] == '.apk':
+                return os.path.abspath(f)
+        return os.path.abspath(path)
 
+
+if __name__ == "__main__":
+    if login(os.environ["LANZOU_ID"], os.environ["LANZOU_PSD"]) == LanZouCloud.SUCCESS:
         # 要存放的文件夹名
         folder_name = os.environ["FOLDER_NAME"]
         print('文件夹名字:', folder_name)
@@ -95,11 +94,8 @@ if __name__ == "__main__":
         folder_id = folders.find_by_name(folder_name).id
         print('文件夹ID:', folder_id)
 
-        flie_path = os.environ["FILE_PATH"]
+        flie_path = get_apk_path(os.environ["FILE_PATH"])
         print('文件路径:', flie_path)
-
-        # 解除大小限制
-        lzy.ignore_limits()
 
         if upload(flie_path, folder_id) == LanZouCloud.SUCCESS:
             # 获取文件夹分享信息
