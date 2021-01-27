8ae2dac04f41b06246a9b6eabffd3e24844a1fcd
Celeter <48249130+Celeter@users.noreply.github.com>
2021-01-27 19:02:27 +0800

    Update API_lanzou.py

--- .github/scripts/API_lanzou.py
+++ .github/scripts/API_lanzou.py
@@ -65,9 +65,11 @@ if __name__ == "__main__":
 
         if upload(flie_path, folder_id) == LanZouCloud.SUCCESS:
             info = lzy.get_share_info(folder_id, is_file=False)
+            print(f'分享链接{info.url}')
             #print('\n分享链接:{}\n提取码:{}'.format(info.url, '无' if info.pwd == '' else info.pwd))
         else:
             print('第一次上传失败，再次上传')
             if upload(flie_path, folder_id) == LanZouCloud.SUCCESS:
                 info = lzy.get_share_info(folder_id, is_file=False)
+                print(f'分享链接{info.url}')
                 #print('\n分享链接:{}\n提取码:{}'.format(info.url, '无' if info.pwd == '' else info.pwd))
